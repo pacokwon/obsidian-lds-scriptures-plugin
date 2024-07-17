d43a14eab3b95ed6942ca3ce264d3b1ee9eee61f
ingiestein <ingie.stein@gmail.com>
2024-07-17 06:47:07 -0600

    remove problematic dependency

--- src/utils/urlparsing.ts
+++ src/utils/urlparsing.ts
@@ -1,6 +1,3 @@
-import { URL } from 'url';
-import queryString from 'query-string';
-
 interface ParsedURL {
     protocol: string;
     hostname: string;
@@ -19,36 +16,39 @@ export function parseURL(url: string): ParsedURL {
     const parsedUrl = new URL(url);
     const pathParts = parsedUrl.pathname.split('/').filter(part => part);
 
-    const queryParams = queryString.parse(parsedUrl.search);
+    const searchParams = new URLSearchParams(parsedUrl.search);
+    const queryParams: { [key: string]: string | undefined } = {};
+    searchParams.forEach((value, key) => {
+        queryParams[key] = value;
+    });
     
     let paragraphs;
     const id = queryParams.id;
     if (typeof id === 'string') {
         let match = id.match(/p(\d+)-p(\d+)/);
         if (match) {
-        paragraphs = {
-            start: parseInt(match[1], 10),
-            end: parseInt(match[2], 10)
-        };
-        } else {
-        match = id.match(/p(\d+)/);
-        if (match) {
             paragraphs = {
-            start: parseInt(match[1], 10),
-            end: parseInt(match[1], 10)
+                start: parseInt(match[1], 10),
+                end: parseInt(match[2], 10)
             };
+        } else {
+            match = id.match(/p(\d+)/);
+            if (match) {
+                paragraphs = {
+                    start: parseInt(match[1], 10),
+                    end: parseInt(match[1], 10)
+                };
+            }
         }
-        }
-        }
-    
+    }
 
     return {
         protocol: parsedUrl.protocol,
         hostname: parsedUrl.hostname,
         pathParts,
         queryParams: {
-        lang: queryParams.lang as string,
-        id: id as string
+            lang: queryParams.lang,
+            id: queryParams.id
         },
         paragraphs
     };
